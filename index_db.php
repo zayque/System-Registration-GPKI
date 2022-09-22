a1f12b0746e1bebe1704b06901aab9105d161c0a
Akmal Hafiz Bin Hashim <47094871+zayque@users.noreply.github.com>
2022-09-22 09:52:29 +0800

    Update index_db.php

--- index_db.php
+++ index_db.php
@@ -93,8 +93,8 @@ if (isset($_POST['submit'])) {
 								    $mail->isSMTP();                                      // Set mailer to use SMTP
 								    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 								    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-								    $mail->Username = 'akmalhafizyakuza@gmail.com';                 // SMTP username
-								    $mail->Password = 'Akmalhafiz321';                           // SMTP password
+								    $mail->Username = 'Your Email';                 // SMTP username
+								    $mail->Password = 'Your Password';                           // SMTP password
 								    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 								    $mail->Port = 587;                                    // TCP port to connect to
 
